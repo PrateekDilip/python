e5ffaf58406f3dc52f3ecdf99a362ce11042f275
Prateek Dilip <halwe.prateek@gmail.com>
2017-11-02 13:59:10 +0530

    server listening to a socket

---
+++ server.py
@@ -0,0 +1,18 @@
+import os, socket
+
+TCP_IP = '127.0.0.1'
+TCP_IP_PORT = 2000
+BUFF_SIZE = 200
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_IP_PORT))
+s.listen(1)
+
+conn, addr = s.accept()
+print "Connection address:", addr
+while 1:
+     data = conn.recv(BUFF_SIZE)
+     if not data: break
+     print "received data:", data
+     conn.send(data)  # echo
+conn.close()
